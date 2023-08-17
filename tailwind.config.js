0f9d036b6fd4cb8c21a77e045239c7d75e4d5093
Vashu <im.vashukashyap@gmail.com>
2023-08-17 19:56:47 +0530

    intial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+        borel: ["Borel", "cursive"],
+      },
+      colors: {
+        "dark-black": "#0B0D17",
+        "tealish-blue": "#D0D6F9",
+        'dark-grey':'#333333',
+        'light-grey':'#ACACAC'
       },
     },
   },
   plugins: [],
-}
+};
